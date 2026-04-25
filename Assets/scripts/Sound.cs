5bd00b5349750dbed400e1f7714e1cb307a0c482
shnseymanr <sahinseymanur123@gmail.com>
2026-04-25 16:20:24 +0300

    button melodi değişimi

--- Assets/scripts/Sound.cs
+++ Assets/scripts/Sound.cs
@@ -5,12 +5,21 @@ public class AudioManager : MonoBehaviour
     [Header("--- Audio Sources ---")]
     [SerializeField] private AudioSource musicSource;  // Müzik için
     [SerializeField] private AudioSource clickSource;  // Click efekti için
-    [SerializeField] private AudioSource effectSource; // Diğer efekt için
+    [SerializeField] private AudioSource effectSource1; // Diğer efekt için
+    [SerializeField] private AudioSource effectSource2; // Diğer efekt için
+    [SerializeField] private AudioSource effectSource3; // Diğer efekt için
+    [SerializeField] private AudioSource effectSource4; // Diğer efekt için
+    [SerializeField] private AudioSource effectSource5; // Diğer efekt için
 
     [Header("--- Audio Clips ---")]
     public AudioClip backgroundMusic;
     public AudioClip clickClip;
-    public AudioClip specialEffectClip;
+    public AudioClip specialEffectClip1;
+    public AudioClip specialEffectClip2;
+    public AudioClip specialEffectClip3;
+    public AudioClip specialEffectClip4;
+    public AudioClip specialEffectClip5;
+
 
     void Start()
     {
@@ -28,8 +37,24 @@ public class AudioManager : MonoBehaviour
     }
 
     // Diğer özel buton efekti için fonksiyon
-    public void PlaySpecialEffect()
+    public void PlaySpecialEffect1()
     {
-        effectSource.PlayOneShot(specialEffectClip);
+        effectSource1.PlayOneShot(specialEffectClip1);
+    }
+    public void PlaySpecialEffect2()
+    {
+        effectSource2.PlayOneShot(specialEffectClip2);
+    }
+    public void PlaySpecialEffect3()
+    {
+        effectSource3.PlayOneShot(specialEffectClip3);
+    }
+    public void PlaySpecialEffect4()
+    {
+        effectSource4.PlayOneShot(specialEffectClip4);
+    }
+    public void PlaySpecialEffect5()
+    {
+        effectSource5.PlayOneShot(specialEffectClip5);
     }
 }
